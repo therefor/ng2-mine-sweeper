18d58a49b50cad0e7fcaf85ec92f949c054d3df9
therefor <wpzhongxt@163.com>
2017-05-03 01:17:16 +0800

    bind child and parent component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,62 @@ import { MineGrid } from './grid.component';
 export class AppComponent {
   xLength: number = 10;
   yLength: number = 10;
+  unCovered={};
+  hasMine={};
+  count={};
+  totalMine: number = 0;
+  flagTag: number = 0; //标记旗子的个数
+  firstClick: boolean = true;
+
+  numArr(x:any){
+    let numArr = [];
+    for (let i = 0; i < x; i++) {
+      numArr.push(i);
+    }
+    return numArr;
+  }
+  coord(x:any, y:any){
+    return x.toString() + y.toString();
+  }
+  leftClick( flag: string){
+    if ( this.firstClick ){
+      //do something to avoid boom at first click
+      this.firstClick = false;
+    }
+    console.log( flag );
+    console.log("child component click left")
+
+  }
+  rightClick( flag: string){
+    console.log( flag );
+    console.log("child component click right")
+    this.flagTag++;
+  }
+  ngOnInit(){
+    //console.log("yers");
+    for ( let i = 0; i < this.xLength; i++){
+      for ( let j = 0; j < this.yLength; j++) {
+        let tempCoord = this.coord(i, j);
+        this.unCovered[tempCoord] = false;
+        this.hasMine[tempCoord] = Math.random() > 0.5? true: false;
+        if( this.hasMine[tempCoord]) { this.totalMine ++; }
+        this.count[tempCoord] = 0;
+      }
+    }
+    for ( let i = 0; i < this.xLength; i++){
+      for ( let j = 0; j < this.yLength; j++) {
+        let countCoord = this.coord(i, j);
+        for( let  x = i-1; x < i+2; x++){
+          for( let y = j-1; y < j+2; y++){
+            let tempCoord = this.coord( x, y );
+            if( this.hasMine.hasOwnProperty(tempCoord) ){
+              if ( this.hasMine[tempCoord] ){
+                this.count[countCoord]++;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
--- src/app/grid.component.ts
+++ src/app/grid.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'mine-grid',
@@ -7,15 +7,26 @@ import { Component } from '@angular/core';
 })
 
 export class MineGrid{
-  private unCovered:boolean = true;
-  private count:number = 8;
+  @Output() leftClick = new EventEmitter<string>();
+  @Output() rightClick = new EventEmitter<string>();
+  @Input() unCovered:boolean;
+  @Input() count:any;
+  @Input() hasMine: boolean;
+  @Input() coord: string;
 
   onClick() { // without type info
+    this.rightClick.emit(this.coord);
     console.log( "right click");
     return false;
   }
   onLeftClick(){
+    this.leftClick.emit(this.coord);
     console.log("left click!");
     return false;
+  }
+  ngOnInit(){
+    if (this.hasMine){
+      this.count = 9;
+    }
   }
 }
